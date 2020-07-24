9d1c5500bd58e32f76a04dcb4ccde9072698ade7
Mark S. Maglana <mark.maglana@canonical.com>
2020-07-24 05:50:49 +0000

    Add fzf

---
+++ dot_fzf.bash
@@ -0,0 +1,7 @@
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "$HOME/.config/fzf-completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "$HOME/.config/fzf-key-bindings.bash"
