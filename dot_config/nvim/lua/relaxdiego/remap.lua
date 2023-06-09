af3118314817b8b407497cc3b9e16e5db86f9d01
Mark Maglana <mmaglana@gmail.com>
2023-06-10 04:33:02 +0800

    Replace search and replace live keymap

--- dot_config/nvim/lua/relaxdiego/remap.lua
+++ dot_config/nvim/lua/relaxdiego/remap.lua
@@ -24,8 +24,8 @@ vim.keymap.set("x", "p", "pgvy")
 vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("v", "<leader>y", '"+y')
 
--- Find and replace live
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- Search and replace live
+vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Make current file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
