3ed142c32fd323dcdefff55cb95494c4a7e20e0e
Mark Maglana <mmaglana@gmail.com>
2023-06-09 21:04:08 +0800

    Traverse telescope results more easily

--- dot_config/nvim/after/plugin/telescope.lua
+++ dot_config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,18 @@
+local actions = require "telescope.actions"
 local builtin = require("telescope.builtin")
+
+require("telescope").setup({
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+      }
+    }
+  },
+})
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
