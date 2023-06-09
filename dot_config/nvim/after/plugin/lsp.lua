043f77d50bca7cab8c3e9e2c78db83173a5d0dee
Mark Maglana <mmaglana@gmail.com>
2023-06-09 21:16:58 +0800

    Reformat lsp.lua

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -5,98 +5,104 @@ lsp.preset("recommended")
 -- LSP list at https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 -- You can also use the :Mason command to interactively install LSPs
 lsp.ensure_installed({
-  "gopls",
-  "pylsp",
+	"gopls",
+	"lua_ls",
+	"pylsp",
 })
 
 -- Fix Undefined global "vim"
 lsp.nvim_workspace()
 
-
 local cmp = require("cmp")
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-  ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-  ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
 })
 
 cmp_mappings["<Tab>"] = nil
 cmp_mappings["<S-Tab>"] = nil
 
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+	mapping = cmp_mappings,
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = "",
-        warn = "",
-        hint = "",
-        info = ""
-    }
+	suggest_lsp_servers = false,
+	sign_icons = {
+		error = "",
+		warn = "",
+		hint = "",
+		info = "",
+	},
 })
 
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+	virtual_text = true,
 })
-
-vim.cmd [[ command! Format execute "lua vim.lsp.buf.format({async=true})" ]]
 
 -- See: https://github.com/python-lsp/python-lsp-server/blob/develop/CONFIGURATION.md
 require("lspconfig").pylsp.setup({
-  settings = {
-    pylsp = {
-      plugins = {
-        pycodestyle = {
-            enabled = false,
-        },
-        pyflakes = {
-            enabled = false,
-        },
-      }
-    }
-  }
+	settings = {
+		pylsp = {
+			plugins = {
+				pycodestyle = {
+					enabled = false,
+				},
+				pyflakes = {
+					enabled = false,
+				},
+			},
+		},
+	},
 })
-vim.api.nvim_exec([[
-  autocmd InsertLeave *.py Format
-]], false)
 
 -- See: https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-config
 require("lspconfig").gopls.setup({})
 
+-- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
+require("lspconfig").lua_ls.setup({})
+
 require("mason-null-ls").setup({
-    ensure_installed = {
-        "black",
-        "blackd-client",
-        "gofumpt",
-        "isort",
-        "jq",
-        "luaformatter",
-        "markdown-toc",
-        "markdownlint",
-        "prettier",
-        "shellcheck",
-        "shfmt",
-        "yamlfmt",
-    },
-    automatic_installation = false,
-    handlers = {}
+	ensure_installed = {
+		"black",
+		"blackd-client",
+		"gofumpt",
+		"isort",
+		"jq",
+		"luaformatter",
+		"markdown-toc",
+		"markdownlint",
+		"prettier",
+		"shellcheck",
+		"shfmt",
+		"yamlfmt",
+	},
+	automatic_installation = false,
+	handlers = {},
 })
 local null_ls = require("null-ls")
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-    debug = true,
-    sources = {
-        formatting.black.with({ extra_args = {"--fast"} }),
-        formatting.isort,
-        formatting.prettier,
-        diagnostics.flake8,
-    }
+	debug = true,
+	sources = {
+		formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.isort,
+		formatting.prettier,
+		diagnostics.flake8,
+	},
 })
+
+vim.cmd([[ command! Format execute "lua vim.lsp.buf.format({async=true})" ]])
+vim.api.nvim_exec(
+	[[
+  autocmd InsertLeave *.* Format
+]],
+	false
+)
