cf4848d717d411bc6319bf4bd11d5cc8af70e7bf
Mark Maglana <mmaglana@gmail.com>
2023-06-09 10:16:15 +0800

    Customize neo -tree

--- dot_config/nvim/after/plugin/neo-tree.lua
+++ dot_config/nvim/after/plugin/neo-tree.lua
@@ -4,6 +4,33 @@ require("neo-tree").setup({
         name = {
             trailing_slash = true
         },
+        icon = {
+            folder_empty = ""
+        },
+        git_status = {
+            symbols = {
+                -- Change type
+                added     = "✚", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+                deleted   = "✖",-- this can only be used in the git_status source
+                renamed   = "",-- this can only be used in the git_status source
+                -- Status type
+                untracked = "",
+                ignored   = "",
+                unstaged  = "",
+                staged    = "",
+                conflict  = "",
+            }
+        },
+    },
+    filesystem = {
+        filtered_items = {
+            visible = true,
+            hide_by_name = {
+              "node_modules"
+            },
+        },
+        hijack_netrw_behavior = "open_default",
     },
 })
 vim.cmd([[nnoremap <leader>nt :Neotree toggle<cr>]])
