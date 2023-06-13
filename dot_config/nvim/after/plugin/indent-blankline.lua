ab4e68741135a90783dc4d5943261222644b6611
Mark Maglana <mmaglana@gmail.com>
2023-06-13 19:08:37 +0800

    Add more contrast to current context

--- dot_config/nvim/after/plugin/indent-blankline.lua
+++ dot_config/nvim/after/plugin/indent-blankline.lua
@@ -1,4 +1,11 @@
+-- Adds indentation guides to all lines (including empty lines).
+vim.cmd [[highlight IndentBlanklineIndent guifg=#333333 gui=nocombine]]
+
 require("indent_blankline").setup({
+  -- Highlight the current indentation
   show_current_context = true,
   show_current_context_start = false,
+  char_highlight_list = {
+    "IndentBlanklineIndent",
+  }
 })
