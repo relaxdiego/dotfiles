bee4f7143ed2790f8f0d56e102817d4d794b426e
Mark Maglana <mmaglana@gmail.com>
2023-06-14 04:46:12 +0800

    Add config autopairs

--- dot_config/nvim/after/plugin/autopairs.lua
+++ dot_config/nvim/after/plugin/autopairs.lua
@@ -1,3 +1,8 @@
+-- See: https://github.com/windwp/nvim-autopairs
 require("nvim-autopairs").setup({
   disable_filetype = { "TelescopePrompt", "vim" },
+  -- Use treesitter to check for a pair
+  check_ts = true,
+  -- Don't add pairs if it already has a close pair in the same line
+  enable_check_bracket_line = true
 })
