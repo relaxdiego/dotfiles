afa9b295d5482567611a141eea3b081a544081e6
Mark Maglana <mmaglana@gmail.com>
2023-06-14 04:33:15 +0800

    Use treesitter for folding

--- dot_config/nvim/after/plugin/treesitter.lua
+++ dot_config/nvim/after/plugin/treesitter.lua
@@ -30,3 +30,6 @@ require("nvim-treesitter.configs").setup({
     additional_vim_regex_highlighting = false,
   },
 })
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false
