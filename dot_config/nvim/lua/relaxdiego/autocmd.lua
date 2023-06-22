8244d53eb391c2774550de830e319d826c9a01e4
Mark Maglana <mmaglana@gmail.com>
2023-06-22 15:20:21 +0800

    Move autocmds to autocmds.lua

--- dot_config/nvim/lua/relaxdiego/autocmd.lua
+++ dot_config/nvim/lua/relaxdiego/autocmd.lua
@@ -1,4 +1,9 @@
+-- Force filetypes for chezmoi templates
 vim.api.nvim_create_autocmd("BufNewFile,BufRead", {
   pattern = "*.sh.tmpl",
   command = "setfiletype sh",
+})
+vim.api.nvim_create_autocmd("BufNewFile,BufRead", {
+  pattern = "*.py.tmpl",
+  command = "setfiletype python",
 })
