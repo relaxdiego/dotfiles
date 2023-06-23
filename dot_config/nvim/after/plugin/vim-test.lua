56090981be671da9fa1d84c9137fcb2d10deae52
Mark Maglana <mmaglana@gmail.com>
2023-06-23 10:44:33 +0800

    Set testing variables

--- dot_config/nvim/after/plugin/vim-test.lua
+++ dot_config/nvim/after/plugin/vim-test.lua
@@ -1,2 +1,8 @@
 -- Run the tests in a tmux pane you specify
 vim.cmd [[let test#strategy = "tslime"]]
+
+if vim.fn.filereadable("pytest.ini") == 1 then
+  vim.g["test#python#runner"] = 'pytest'
+end
+
+vim.g["test#shell#bats#options"] = "--recursive"
