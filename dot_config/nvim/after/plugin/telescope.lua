8208355c7a280440916b1ffe4058100a72dd8dbe
Mark Maglana <mmaglana@gmail.com>
2023-06-28 07:09:58 +0800

    Use telescope to find a buffer

--- dot_config/nvim/after/plugin/telescope.lua
+++ dot_config/nvim/after/plugin/telescope.lua
@@ -17,6 +17,6 @@ require("telescope").setup({
 
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>ag", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>c", builtin.buffers, { desc = "Find buffer" })
 -- Others:
--- vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 -- vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
