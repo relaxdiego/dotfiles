a1fce208b31373f9d63ad8b7a6a85fc912343f26
Mark Maglana <mmaglana@gmail.com>
2023-07-15 16:24:34 +0800

    Use fork of alpha-nvim

--- dot_config/nvim/lua/relaxdiego/plugins/alpha-nvim.lua
+++ dot_config/nvim/lua/relaxdiego/plugins/alpha-nvim.lua
@@ -1,10 +1,10 @@
 return {
-	"goolord/alpha-nvim",
-	commit = "9e33db324b8bb7a147bce9ea5496686ee859461d",
-	event = "VimEnter",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = function()
-		local theme = require("relaxdiego.theme")
-		return theme.config
-	end,
+    "relaxdiego/alpha-nvim",
+    commit = "b0f1744",
+    event = "VimEnter",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = function()
+        local theme = require("relaxdiego.theme")
+        return theme.config
+    end,
 }
