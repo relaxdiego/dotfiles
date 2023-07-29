6669bc15695f2d4030a3c968a78a4a25411e8db6
Mark Maglana <mmaglana@gmail.com>
2023-07-29 09:25:42 +0800

    Apply lua formatter

--- dot_config/nvim/lua/relaxdiego/theme/init.lua
+++ dot_config/nvim/lua/relaxdiego/theme/init.lua
@@ -3,11 +3,11 @@ require("alpha.term")
 
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.buttons.val = {
-	dashboard.button("Ctrl p", "󰈞  Find file", "<C-P>"),
-	dashboard.button("<leader> ag", "󰑑  Live Grep"),
-	dashboard.button("<leader> nt", "󰙅  Tree view"),
-	dashboard.button("e", "  New file", "<cmd>ene <CR>"),
-	dashboard.button("(Sorry, you're stuck)", "󰩈  Quit", ":q<CR>"),
+    dashboard.button("Ctrl p", "󰈞  Find file", "<C-P>"),
+    dashboard.button("<leader> ag", "󰑑  Live Grep"),
+    dashboard.button("<leader> nt", "󰙅  Tree view"),
+    dashboard.button("e", "  New file", "<cmd>ene <CR>"),
+    dashboard.button("(Sorry, you're stuck)", "󰩈  Quit", ":q<CR>"),
 }
 
 -- get list of files that match the pattern '*.sh'
@@ -21,38 +21,38 @@ local win_id = vim.api.nvim_get_current_win()
 local height = vim.api.nvim_win_get_height(win_id)
 
 if height > 30 then
-	dashboard.section.terminal.command = "cat | " .. art_file
-	dashboard.section.terminal.width = 25
-	dashboard.section.terminal.height = 13
+    dashboard.section.terminal.command = "cat | " .. art_file
+    dashboard.section.terminal.width = 25
+    dashboard.section.terminal.height = 13
 else
-	dashboard.section.terminal.height = 1
+    dashboard.section.terminal.height = 1
 end
 
 local smol = {
-	"█▄░█ █▀▀ █▀█ █░█ █ █▀▄▀█",
-	"█░▀█ ██▄ █▄█ ▀▄▀ █ █░▀░█",
+    "█▄░█ █▀▀ █▀█ █░█ █ █▀▄▀█",
+    "█░▀█ ██▄ █▄█ ▀▄▀ █ █░▀░█",
 }
 
 local header = {
-	type = "text",
-	val = smol,
-	opts = {
-		position = "center",
-		hl = "Type",
-	},
+    type = "text",
+    val = smol,
+    opts = {
+        position = "center",
+        hl = "Type",
+    },
 }
 
 dashboard.section.header = header
 
 dashboard.config.layout = {
-	{ type = "padding", val = 1 },
-	dashboard.section.terminal,
-	{ type = "padding", val = 1 },
-	dashboard.section.header,
-	{ type = "padding", val = 2 },
-	dashboard.section.buttons,
-	{ type = "padding", val = 1 },
-	dashboard.section.footer,
+    { type = "padding", val = 1 },
+    dashboard.section.terminal,
+    { type = "padding", val = 1 },
+    dashboard.section.header,
+    { type = "padding", val = 2 },
+    dashboard.section.buttons,
+    { type = "padding", val = 1 },
+    dashboard.section.footer,
 }
 
 return dashboard
