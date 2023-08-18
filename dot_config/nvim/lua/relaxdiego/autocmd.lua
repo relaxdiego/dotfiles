a15ebdf96c661576bf0d3e84f5f9074ab0c5edec
Mark Maglana <mmaglana@gmail.com>
2023-08-18 09:33:35 +0800

    Format update

--- dot_config/nvim/lua/relaxdiego/autocmd.lua
+++ dot_config/nvim/lua/relaxdiego/autocmd.lua
@@ -1,3 +1,4 @@
 -- Force filetypes for chezmoi templates
-vim.api.nvim_command('autocmd BufNewFile,BufRead *.sh.tmpl setfiletype sh')
-vim.api.nvim_command('autocmd BufNewFile,BufRead *.py.tmpl setfiletype python')
+vim.api.nvim_command("autocmd BufNewFile,BufRead *.sh.tmpl setfiletype sh")
+vim.api.nvim_command("autocmd BufNewFile,BufRead *.py.tmpl setfiletype python")
+vim.api.nvim_command("autocmd BufNewFile,BufRead *.y*ml.tmpl setfiletype yaml")
