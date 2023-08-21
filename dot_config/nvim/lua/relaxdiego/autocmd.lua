37b0e755b568bcbb4437ca4bb7a62631702b9199
Mark Maglana <mmaglana@gmail.com>
2023-08-21 13:48:17 +0800

    Set template filetype

--- dot_config/nvim/lua/relaxdiego/autocmd.lua
+++ dot_config/nvim/lua/relaxdiego/autocmd.lua
@@ -2,3 +2,4 @@
 vim.api.nvim_command("autocmd BufNewFile,BufRead *.sh.tmpl setfiletype sh")
 vim.api.nvim_command("autocmd BufNewFile,BufRead *.py.tmpl setfiletype python")
 vim.api.nvim_command("autocmd BufNewFile,BufRead *.y*ml.tmpl setfiletype yaml")
+vim.api.nvim_command("autocmd BufNewFile,BufRead *.y*ml.template setfiletype yaml")
