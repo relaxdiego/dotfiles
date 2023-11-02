9670f540b9b94f9e78753bd2d96ac9dfb4c4a9e7
Mark Maglana <mmaglana@gmail.com>
2023-11-02 16:12:55 +0800

    Apply formatting

--- dot_config/nvim/lua/relaxdiego/plugins/autopairs.lua
+++ dot_config/nvim/lua/relaxdiego/plugins/autopairs.lua
@@ -1,14 +1,14 @@
 return {
-	"windwp/nvim-autopairs",
-	commit = "59df87a84c80a357ca8d8fe86e451b93ac476ccc",
-	config = function()
-		-- See: https://github.com/windwp/nvim-autopairs
-		require("nvim-autopairs").setup({
-			disable_filetype = { "TelescopePrompt", "vim" },
-			-- Use treesitter to check for a pair
-			check_ts = true,
-			-- Don't add pairs if it already has a close pair in the same line
-			enable_check_bracket_line = true,
-		})
-	end,
+    "windwp/nvim-autopairs",
+    commit = "59df87a84c80a357ca8d8fe86e451b93ac476ccc",
+    config = function()
+        -- See: https://github.com/windwp/nvim-autopairs
+        require("nvim-autopairs").setup({
+            disable_filetype = { "TelescopePrompt", "vim" },
+            -- Use treesitter to check for a pair
+            check_ts = true,
+            -- Don't add pairs if it already has a close pair in the same line
+            enable_check_bracket_line = true,
+        })
+    end,
 }
