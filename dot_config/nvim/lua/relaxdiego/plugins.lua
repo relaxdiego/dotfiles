a1543e7a290a5dc4cd7dad9889f97884b5f17d02
Mark Maglana <mmaglana@gmail.com>
2024-05-30 14:00:06 +0800

    style: Apply autoformatting to lua files

--- dot_config/nvim/lua/relaxdiego/plugins.lua
+++ dot_config/nvim/lua/relaxdiego/plugins.lua
@@ -1,21 +1,21 @@
 -- Ensure lazy.nvim is installed and set up
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.fn.system({
-	"git",
-	"-C", -- Execute the command in the provided directory
-	lazypath, -- The directory to execute the command in
-	"checkout",
-	"de0a911", -- v9.25.0
+    "git",
+    "-C",      -- Execute the command in the provided directory
+    lazypath,  -- The directory to execute the command in
+    "checkout",
+    "de0a911", -- v9.25.0
 })
 vim.opt.rtp:prepend(lazypath)
 
@@ -24,8 +24,8 @@ local dir = vim.fn.stdpath("config") .. "/lua/relaxdiego/plugins/"
 local files = vim.fn.globpath(dir, "*.lua", false, true)
 
 for _, file in ipairs(files) do
-	local module = file:match("([^/]+)%.lua$")
-	table.insert(plugins, require("relaxdiego.plugins." .. module))
+    local module = file:match("([^/]+)%.lua$")
+    table.insert(plugins, require("relaxdiego.plugins." .. module))
 end
 
 require("lazy").setup(plugins)
