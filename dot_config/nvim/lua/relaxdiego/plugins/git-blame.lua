b8065cdbf72b6747f73db9d821c4ad59a6dd083e
Mark Maglana <mmaglana@gmail.com>
2024-06-06 09:55:29 +0800

    feat(nvim): Add git-blame.nvim plugin

---
+++ dot_config/nvim/lua/relaxdiego/plugins/git-blame.lua
@@ -0,0 +1,9 @@
+return {
+    "f-person/git-blame.nvim",
+    commit = "408d5487d908dfe5d48e5645d8b27ddcc16b11e0",
+    config = function()
+        require("gitblame").setup({
+            -- See: Config options <https://github.com/f-person/git-blame.nvim?tab=readme-ov-file#configuration>
+        })
+    end,
+}
