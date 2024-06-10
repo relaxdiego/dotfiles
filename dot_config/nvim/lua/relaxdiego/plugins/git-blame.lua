55b359e080e646df32fbb594bc4d28e21f78654a
Mark Maglana <mmaglana@gmail.com>
2024-06-10 16:46:25 +0800

    fix(nvim): Set the minimum column for the blame message

--- dot_config/nvim/lua/relaxdiego/plugins/git-blame.lua
+++ dot_config/nvim/lua/relaxdiego/plugins/git-blame.lua
@@ -4,6 +4,11 @@ return {
     config = function()
         require("gitblame").setup({
             -- See: Config options <https://github.com/f-person/git-blame.nvim?tab=readme-ov-file#configuration>
+
+            -- Have the blame message start, at a minimum, on the given column.
+            -- If the line is longer than the given value, the blame message will
+            -- start at EOL.
+            virtual_text_column = 40
         })
     end,
 }
