d81fd141f50b62f6d9090806d02b9ef86cbf2977
Mark Maglana <mmaglana@gmail.com>
2024-07-18 17:07:22 +0800

    feat(pyright): use 'standard' mode

--- dot_config/nvim/lua/relaxdiego/plugins/lsp-zero.lua
+++ dot_config/nvim/lua/relaxdiego/plugins/lsp-zero.lua
@@ -250,8 +250,8 @@ return {
                 },
                 python = {
                     analysis = {
-                        ignore = {".venv"},
-                        typeCheckingMode = "basic",
+                        ignore = { ".venv" },
+                        typeCheckingMode = "standard",
                     },
                 },
             },
