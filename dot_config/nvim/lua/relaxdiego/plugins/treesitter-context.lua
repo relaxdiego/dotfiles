0e961b21ffb738b2046b0dc71ff10aab6c12ff64
Mark Maglana <mmaglana@gmail.com>
2025-03-18 17:39:19 +0800

    refactor(context): limit space occuppied by window

--- dot_config/nvim/lua/relaxdiego/plugins/treesitter-context.lua
+++ dot_config/nvim/lua/relaxdiego/plugins/treesitter-context.lua
@@ -1,4 +1,10 @@
 return {
     "nvim-treesitter/nvim-treesitter-context",
-    commit = "f6c99b64111ab1424c8fde3d9a6f3cd08234f8cb"
+    commit = "f6c99b64111ab1424c8fde3d9a6f3cd08234f8cb",
+    config = function()
+        require("treesitter-context").setup({
+            max_lines = 3,
+            min_window_height = 30,
+        })
+    end,
 }
